484d54d1653dabf8a544d7a456502a7742051a00
Teo <tp4348@student.uni-lj.si>
2021-03-17 21:22:08 +0100

    commit ffdfa3f300db1fc40059b3672853c2da48aba746 Author: mbostic <bosticmatjaz@gmail.com> Date:   Tue Mar 16 18:28:34 2021 +0100
        Added script which edits 'cfg/single_config.yaml' file

---
+++ scripts/cfg_edit.py
@@ -0,0 +1,97 @@
+import yaml
+import os
+import sys
+import imp
+import rospkg
+
+em = None
+for path in sys.path:
+    filename = os.path.join(path, 'em.py')
+    if os.path.exists(filename):
+        em = imp.load_source('em', filename)
+        if "expand" in dir(em):
+            break
+# For-else: else is called if loop doesn't break
+else:
+    print(
+        "ERROR: could not find module em, please sudo apt install python-empy")
+    exit(2)
+
+
+if __name__ == '__main__':
+
+    r = rospkg.RosPack()
+    defaultCfgPath = os.path.join(r.get_path('stag_ros'), "cfg/single_config.yaml")
+
+    while True:
+        try:
+            cfgPath = str(raw_input("Enter the path to the single_config.yaml (or leave empty for default - %s): --> " % defaultCfgPath))
+            if not cfgPath:
+                cfgPath = defaultCfgPath
+            if not os.path.isfile(cfgPath):
+                print("Sorry, there is no file on specified path.")
+                continue
+        except (ValueError, NameError, SyntaxError):
+            print("Sorry, your input was wrong.")
+            continue
+        else:
+            break
+
+    while True:
+        try:
+            numMarkers = input("Enter the number of markers: --> ")
+        except (ValueError, NameError, SyntaxError):
+            print("Sorry, your input was wrong.")
+            continue
+        else:
+            break
+
+    while True:
+        try:
+            markerSize = input("Enter the size of markers in meters: --> ")
+        except (ValueError, NameError, SyntaxError):
+            print("Sorry, your input was wrong.")
+            continue
+        else:
+            break
+
+    halfMarkerSize = markerSize / 2.0
+
+    inner = ""
+
+    for i in range(1, numMarkers + 1):
+
+        s = em.expand("""    {
+      frame: "tag_@(id)",
+      id: @(id),
+      corners: [
+        [-@(halfMarkerSize), @(halfMarkerSize), 0.0],
+        [@(halfMarkerSize), @(halfMarkerSize), 0.0],
+        [@(halfMarkerSize), -@(halfMarkerSize), 0.0]
+      ]
+    },
+""", {"id": i, "halfMarkerSize": halfMarkerSize})
+
+        inner += s
+
+    inner = inner[:-2]  # remove final ",\n"
+
+    out = em.expand("""tags:
+  [
+@(str)
+  ]
+
+# Marker corner order:
+# 1          2
+#  HD########
+#  #ID#  ####
+#  ##  ##  ##
+#  ####  ####
+#  ##########
+# 4          3
+
+    """, {"str": inner})
+
+    f = open(defaultCfgPath, "w")
+    f.write(out)
+    f.close()
